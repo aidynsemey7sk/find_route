798166d9bed0c3b3d98046e329b5a77b352e2bd4
aidyn <aidynsemey7sk@gmail.com>
2022-02-19 21:41:47 +0600

    Тестовая вьюха

--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 
@@ -8,10 +9,16 @@ __all__=(
     )
 
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     if pk:
         city = get_object_or_404(City, id=pk)
         context = {'objects_list': city}
         return render(request, 'cities/detail.html', context)
+    form = CityForm()
     cities = City.objects.raw("SELECT * FROM cities_city")
-    context = {'objects_list': cities}
+    context = {'objects_list': cities, 'form': form}
     return render(request, 'cities/home.html', context)

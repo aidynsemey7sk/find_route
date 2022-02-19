e6896fa75ed86a8a449fff2b9025336b5ca9f6de
aidyn <aidynsemey7sk@gmail.com>
2022-02-19 15:02:50 +0600

    Добавление детализации модели городов

--- cities/urls.py
+++ cities/urls.py
@@ -5,4 +5,5 @@ from cities.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('<int:pk>/', home, name='home'),
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from cities.models import City
 
@@ -7,7 +7,11 @@ __all__=(
     'home',
     )
 
-def home(request):
-    cities = City.objects.all()
+def home(request, pk=None):
+    if pk:
+        city = get_object_or_404(City, id=pk)
+        context = {'objects_list': city}
+        return render(request, 'cities/detail.html', context)
+    cities = City.objects.raw("SELECT * FROM cities_city")
     context = {'objects_list': cities}
     return render(request, 'cities/home.html', context)

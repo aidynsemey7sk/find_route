3a843e10716c5c6bab7085583fdc7d1722a5d956
aidyn <aidynsemey7sk@gmail.com>
2022-02-20 19:24:15 +0600

    Добавление сообщений об успешном CRUD

--- cities/views.py
+++ cities/views.py
@@ -3,7 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     DetailView, CreateView, UpdateView, DeleteView, ListView
     )
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
+from django.contrib import messages
 
 from cities.forms import CityForm
 from cities.models import City
@@ -41,18 +43,20 @@ class CityDetailView(DetailView):
     template_name = 'cities/detail.html'
 
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно создан'
 
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно отредактирован'
 
 
 class CityDeleteView(DeleteView):
@@ -61,6 +65,7 @@ class CityDeleteView(DeleteView):
     success_url = reverse_lazy('cities:home')
     
     def get(self, request, *args, **kwargs) :
+        messages.success(request, 'Город успешно удален.')
         return self.post(request, *args, **kwargs)
 
 
